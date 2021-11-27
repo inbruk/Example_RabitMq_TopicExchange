083c4cf87d1f032232431ac97458de9b690e1e34
inbruk <35333872+inbruk@users.noreply.github.com>
2021-11-28 00:49:37 +0300

    .

---
+++ Producer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        private static readonly List<String> brands = new List<String>() { "BMW", "Audi", "Tesla", "Hyundai" };
+        private static readonly List<String> colors = new List<String>() { "red", "green", "blue", "white" };
+        private static readonly List<String> fuels = new List<String>() { "gasoline", "gas", "electric", "diesel" };
+        private static readonly Random rnd = new Random();
+
+        static String GenerateCarRoutingKey()
+        {
+            String brand = brands[rnd.Next(0,3)];
+            String color = colors[rnd.Next(0,3)];
+            String fuel = fuels[rnd.Next(0,3)];
+
+            if (brand == brands[2])
+                fuel = fuels[2];
+
+            String routingKey = $"{brand}.{color}.{fuel}";
+            return routingKey;
+        }
+
+
+        static Func<Task> CreateTask()
+        {
+            return () =>
+            {
+                var counter = 0;
+                do
+                {
+                    var timeToSleep = rnd.Next(1000, 3000);
+                    Thread.Sleep(timeToSleep);
+
+                    String routingKey = GenerateCarRoutingKey();
+
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare("topic_logs", ExchangeType.Topic);
+
+                        var message = $"'{routingKey}' message from producer N:{counter++}";
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "direct_logs", routingKey: routingKey, basicProperties: null, body: body);
+
+                        Console.WriteLine(message);
+                    }
+                }
+                while (true);
+            };
+        }
+
+        static void Main(String[] args)
+        {
+            Task.Run(CreateTask());
+            Console.ReadKey();
+        }
+    }
+}
